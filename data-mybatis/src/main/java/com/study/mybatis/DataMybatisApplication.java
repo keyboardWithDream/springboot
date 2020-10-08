07fde5a9f3f1ba071c1f60512e180dde7bf8d62a
keyboardWithDream <isharlan.hu@gmail.com>
2020-10-08 14:31:49 +0800

    整合MyBatis初始化

---
+++ data-mybatis/src/main/java/com/study/mybatis/DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.study.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMybatisApplication.class, args);
+	}
+
+}
